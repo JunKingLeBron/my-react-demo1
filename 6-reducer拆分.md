86a7ff984f489c7aaa54416f32de1091796e749a
liwei <elvmx47@aliyun.com>
2019-03-04 10:18:26 +0800

    reducer 的拆分

---
+++ 6-reducer拆分.md
@@ -0,0 +1,5 @@
+# reducer 拆分
+
+> 解决 reducer 文件维护困难的问题。
+
+- 对现在的代码，将 reduers/index.js  拆分  todo.js   cart.js  product.js
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,111 +1,114 @@
 // 主的 reducer
-import { ADD_TODO, ADD_CART, JIAN_CART } from '../actions/types';
+// combineReducers 帮我们做合并
+import { combineReducers } from 'redux';
 
-// 初始化的状态
-let defaultState = {
-  todos: [
-    {
-      text: '吃饭',
-      isOk: false
-    },
-    {
-      text: '睡觉',
-      isOk: true
-    }
-  ], // TodoList 需要去遍历出来的东西
+import todo from './todo';
+import cart from './cart';
+import product from './product';
 
-  // 商品列表
-  products: [
-    {
-      id: 1000,
-      name: '猫粮',
-      price: 10,
-    },
-    {
-      id: 1001,
-      name: '狗粮',
-      price: 20,
-    }
-  ],
+export default combineReducers({
+  todos: todo,
+  carts: cart,
+  products: product
+})
 
-  // 购物车数据
-  carts: [
+// export default (state = {}, action) => {
 
-  ]
-}
+//   /**
+//    * 这里return出去的就是 整个 仓库的 state 数据
+//    */
+//   // return {
+//   //   todos: todo(state.todos, action),
+//   //   // todos: [],
+//   //   carts: cart(state.carts, action),
+//   //   products: product(state.products, action),
+//   // }
 
-let hhh = (prevState = defaultState, action) => {
-  console.log('之前的状态:', prevState);
-  console.log('执行的动作:', action);
-  // 执行深拷贝
-  let state = JSON.parse(JSON.stringify(prevState));
+// }
 
-  switch (action.type) {
-    case ADD_TODO:
-      // 下面这样，违背了纯函数的使用
-      // state.todos.push({
-      //   text: action.text
-      // })
-      state.todos.push({
-        text: action.text,
-        isOk: false
-      })
-      break;
 
-    case 'OK_TODO':
-      state.todos[action.index].isOk = !state.todos[action.index].isOk;
-      break;
+// // 1. 初始化
+//   state.todos undefined   [{吃饭}, {睡觉}]
 
-    case ADD_CART:
-      // 判断当前商品是否存在购物车中，
-      var id = action.product.id; // 商品id
-      var curProduct = state.carts.find(item => {
-        return item.id === id;
-      })
+// // 2. 点击页面中的 add 按钮
+//   state.todos [{吃饭}, {睡觉}]   [{吃饭}, {睡觉}, {打豆豆}]
 
-      if (curProduct) {
-        // 存在数量加1
-        curProduct.num++;
-        curProduct.total = curProduct.price * curProduct.num;
-      } else {
-        // 不存在
-        state.carts.push({
-          ...action.product,
-          num: 1,
-          total: action.product.price
-        });
-      }
-      break;
 
-    case JIAN_CART:
-      // 判断当前商品是否存在购物车中，
-      var id = action.product.id; // 商品id
-      var curProduct = state.carts.find(item => {
-        return item.id === id;
-      })
 
-      var index = state.carts.indexOf(curProduct);
 
-      if (curProduct) {
-        // 存在
 
-        if (curProduct.num > 1) {
-          // 数量大于1的时候，做减法
-          curProduct.num--;
-          curProduct.total = curProduct.price * curProduct.num;
-        } else {
-          // 删除
-          state.carts.splice(index, 1);
-        }
+// let hhh = (prevState = defaultState, action) => {
+//   console.log('之前的状态:', prevState);
+//   console.log('执行的动作:', action);
+//   // 执行深拷贝
+//   let state = JSON.parse(JSON.stringify(prevState));
 
-      }
+//   switch (action.type) {
+//     case ADD_TODO:
+//       // 下面这样，违背了纯函数的使用
+//       // state.todos.push({
+//       //   text: action.text
+//       // })
+//       state.todos.push({
+//         text: action.text,
+//         isOk: false
+//       })
+//       break;
 
-    default:
-      break;
-  }
-  console.log('操作完成之后：', state);
-  // 必须要return
-  return state;
-}
+//     case 'OK_TODO':
+//       state.todos[action.index].isOk = !state.todos[action.index].isOk;
+//       break;
 
-export default hhh;
+//     case ADD_CART:
+//       // 判断当前商品是否存在购物车中，
+//       var id = action.product.id; // 商品id
+//       var curProduct = state.carts.find(item => {
+//         return item.id === id;
+//       })
+
+//       if (curProduct) {
+//         // 存在数量加1
+//         curProduct.num++;
+//         curProduct.total = curProduct.price * curProduct.num;
+//       } else {
+//         // 不存在
+//         state.carts.push({
+//           ...action.product,
+//           num: 1,
+//           total: action.product.price
+//         });
+//       }
+//       break;
+
+//     case JIAN_CART:
+//       // 判断当前商品是否存在购物车中，
+//       var id = action.product.id; // 商品id
+//       var curProduct = state.carts.find(item => {
+//         return item.id === id;
+//       })
+
+//       var index = state.carts.indexOf(curProduct);
+
+//       if (curProduct) {
+//         // 存在
+
+//         if (curProduct.num > 1) {
+//           // 数量大于1的时候，做减法
+//           curProduct.num--;
+//           curProduct.total = curProduct.price * curProduct.num;
+//         } else {
+//           // 删除
+//           state.carts.splice(index, 1);
+//         }
+
+//       }
+
+//     default:
+//       break;
+//   }
+//   console.log('操作完成之后：', state);
+//   // 必须要return
+//   return state;
+// }
+
+// export default hhh;
